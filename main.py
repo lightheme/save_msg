15297e926eefb6a77fa9db789d0b71cd6b5afc6a
ALEXANDER <157208506+alexunderlong@users.noreply.github.com>
2025-04-06 21:44:50 +0300

    1.1 update

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from pystyle import Colorate, Colors
+
+print('[INFO] Приложение запускается...')
+from colorama import init, Fore, Back, Style
 import asyncio
 import json
+import msvcrt
 import re
+from getpass import getpass
 import sys
-
+from telethon.tl.types import PeerChannel
+import menu as dumb_menu
 from tqdm.asyncio import tqdm as async_tqdm
 import glob
 import os
@@ -13,7 +20,7 @@ from tqdm import tqdm
 
 api_id = 20188960
 api_hash = "429cb9b90b420cf6ae70df4979c6a0fe"
-
+init()
 client = TelegramClient('login',
                             api_id,
                             api_hash,
@@ -22,7 +29,8 @@ client = TelegramClient('login',
                             lang_code='ru',
                             app_version='Telegram Android 10.6.5')
 global_chats = {}
-
+supported_symbols = '''ёйцукенгшщзхъфывапролджэячсмитьбю'''+'''ёйцукенгшщзхъфывапролджэячсмитьбю'''.upper()
+action_options = ['Сохранить из новых чатов ', 'Сохранить из уже существующих чатов', 'Выйти']
 
 def beautiful_print(d, offset):
     for i in range(1, 11):
@@ -38,7 +46,8 @@ def beautiful_print(d, offset):
     print()
 
 def filter_ascii(text):
-    return ''.join([i if ord(i) < 128 else ' ' for i in text])
+    filtered_text = ''.join([i if ord(i) < 128 or i in supported_symbols else '' for i in text])
+    return filtered_text if len(filtered_text.replace(' ', "")) > 0 else 'NO KEY TABLE'
 
 async def aenumerate(aiterable):
     i = 0
@@ -67,7 +76,7 @@ def header():
           '\___ \   / _ \   \ \ / / |  _|  | |_) |\n'
           ' ___) | / ___ \   \ V /  | |___ |  _ <\n'
           '|____/ /_/   \_\   \_/   |_____||_| \_\\')
-    print('    '*4+'By Voynov 2025'+'\n\n')
+    print('    ' * 4 + Fore.RED + 'By Voynov 2025 1.1.0' + Style.RESET_ALL + '\n\n')
 
 async def save_msgs(entity, off_id=0):
     header()
@@ -87,7 +96,7 @@ async def save_msgs(entity, off_id=0):
     pbar = tqdm(total=len(msgs))
     for msg in msgs:
         if msg.text:
-            with open(f'{dirname}/{msg.date.strftime("%d-%m-%Y_%H-%M-%S")}_{msg.id}.txt', 'w', encoding='UTF-8') as f:
+            with open(f'{dirname}/{msg.date.strftime("%Y-%m-%d_%H-%M-%S")}_{msg.id}.txt', 'w', encoding='UTF-8') as f:
                 f.write(msg.text)
     pbar.close()
     print('[INFO] Все сообщения были записаны')
@@ -101,10 +110,8 @@ def get_chats():
 async def try_input(text=''):
     try:
         return input(text)
-    except (KeyboardInterrupt, RuntimeError):
-        await client.disconnect()
-        asyncio.get_event_loop().stop()
-        client.loop.stop()
+    except (KeyboardInterrupt, RuntimeError, AttributeError):
+        await disconnectng()
 
 def set_chat(chat_name, chat_id):
     chats = get_chats()
@@ -114,25 +121,52 @@ def set_chat(chat_name, chat_id):
         json.dump(chats, f)
 
 
-async def parse_new_chats(offset=0):
-    header()
-    beautiful_print(global_chats, offset)
-    print('a) Предыдущие        0) Выход        d) Следующие\n\n')
-    input_chats = await try_input('Выберите чат или чаты(написать через пробел): ')
-    if input_chats.lower() == 'a':
-        offset = 1 if offset==0 else offset
-        return await parse_new_chats(offset - 1)
-    elif input_chats.lower() == 'd':
-        return await parse_new_chats(offset + 1)
+async def parse_new_chats():
+    while True:
+        def real_time_input(prompt='', input_str='', offset=0):
+            header()
+            beautiful_print(global_chats, offset)
+            print(Back.LIGHTGREEN_EX+Fore.BLACK+'a) Предыдущая        0) НАЗАД        d) Следующая \n\n'+Style.RESET_ALL)
+            sys.stdout.write(prompt)
+            sys.stdout.write(input_str)
+            sys.stdout.flush()
 
-    if not input_chats.replace(' ', "").isdigit():
-        return
-    if input_chats == '0':
-        return
-    choose_chats = [global_chats[int(inp)] for inp in input_chats.split(' ')]
-    for chat in choose_chats:
-        set_chat(chat[1], chat[0].id)
-        await save_msgs(chat)
+            while True:
+                # py_win_keyboard_layout.change_foreground_window_keyboard_layout(0x04090409)
+                char = msvcrt.getwch()
+                if char == '\r':
+                    sys.stdout.write('\n')
+                    break
+                elif char == '\x08':
+                    if len(input_str) > 0:
+                        input_str = input_str[:-1]
+                        sys.stdout.write('\b \b')
+                elif char.lower() in ('k', 'a', 'ф'):
+                    os.system('cls')
+                    offset = 1 if offset == 0 else offset
+                    return real_time_input(prompt, input_str, offset - 1)
+                elif char.lower() in ('m', 'd', 'в'):
+                    os.system('cls')
+                    return real_time_input(prompt, input_str, offset + 1)
+                else:
+                    if char != 'à':
+                        sys.stdout.write(char)
+                        input_str += char
+
+                sys.stdout.flush()
+            return input_str
+        input_chats = real_time_input('Выберите чат или чаты(написать через пробел): ')
+
+        if not input_chats.replace(' ', "").isdigit():
+            return
+        if input_chats == '0':
+            return
+        choose_chats = [global_chats[int(inp)] for inp in input_chats.split(' ')]
+        for chat in choose_chats:
+            if chat[1] == 'NO KEY TABLE':
+                continue
+            set_chat(chat[1], chat[0].id)
+            await save_msgs(chat)
 
 
 async def find_chat(chat):
@@ -145,7 +179,10 @@ async def find_chat(chat):
         ))
 
         if result.my_results[0]:
-            set_chat(chat, result.my_results[0].chat_id)
+            if isinstance(result.my_results[0], PeerChannel):
+                set_chat(chat, result.my_results[0].channel_id)
+            else:
+                set_chat(chat, result.my_results[0].chat_id)
 
         return result.my_results[0]
     else:
@@ -153,22 +190,26 @@ async def find_chat(chat):
 
 
 async def parse_saved_chats():
-    workdirs = list(filter(lambda x: x.endswith('(saver)'), next(os.walk('.'))[1]))
+    while True:
+        workdirs = list(filter(lambda x: x.endswith('(saver)'), next(os.walk('.'))[1]))
 
-    header()
-    for i, dir in enumerate(workdirs):
-        print(f'{i+1}) {dir.replace("(saver)", "")}')
+        header()
+        for i, dir in enumerate(workdirs):
+            print(f'{i+1}) {dir.replace("(saver)", "")}')
+        print(Back.LIGHTGREEN_EX+Fore.BLACK+'\nEnter) Выбрать все'+'        '+'0) НАЗАД \n\n'+Style.RESET_ALL)
 
-    input_chats = await try_input('Выберите чат или чаты(написать через пробел): ')
-    if input_chats == '0':
-        return
-    choose_chats = [[await find_chat(workdirs[int(inp) - 1]), workdirs[int(inp) - 1]] for inp in input_chats.split(' ')]
-    for chat in choose_chats:
-        await save_msgs(chat)
+        input_chats = await try_input('Выберите чат или чаты(написать через пробел): ')
+        if input_chats == '0':
+            return
+        if input_chats == '':
+            choose_chats = [[await find_chat(workdir), workdir] for workdir in workdirs]
+        else:
+            choose_chats = [[await find_chat(workdirs[int(inp) - 1]), workdirs[int(inp) - 1]] for inp in input_chats.strip(' ').split(' ')]
+        for chat in choose_chats:
+            await save_msgs(chat)
 
 
 async def main():
-    await client.start()
     if not os.path.isfile('saver.config.json'):
         with open('saver.config.json', "w") as f:
             json.dump({}, f)
@@ -183,26 +224,39 @@ async def main():
 
     while True:
         header()
-        print(f'1) Сохранить из новых чатов \n2) Сохранить из уже существующих чатов')
-        choose = await try_input('Выберите вариант (1/2): ')
-
-        if choose == '1':
+        try:
+            menu_entry_index = dumb_menu.get_menu_choice(action_options, isclean = True)+1
+        except KeyboardInterrupt:
+            await disconnectng()
+            return
+        if menu_entry_index == 1:
             await parse_new_chats()
-        elif choose == '2':
+        elif menu_entry_index == 2:
             await parse_saved_chats()
+        elif menu_entry_index == 3:
+            await disconnectng()
+            return
 
         os.system('cls')
+
+async def disconnectng():
+    await client.disconnect()
+    asyncio.get_event_loop().stop()
+    client.loop.stop()
+    os.system('cls')
 
 
 if __name__ == '__main__':
     try:
-        client.start()
+        client.start(
+            phone=lambda: input('Введите номер телефона в формате 7 XXX XXX XX XX: '),
+            password=lambda: getpass('Введите облачный пароль: '),
+            code_callback=lambda: input('Введите код который вам пришел: ')
+        )
         client.loop.create_task(main())
         client.run_until_disconnected()
     except sqlite3.OperationalError:
         for i in glob.glob(u'*.session'):
             os.unlink(i)
     except RuntimeError:
-        asyncio.run(client.disconnect())
-        asyncio.get_event_loop().stop()
-        client.loop.stop()
+        asyncio.run(disconnectng())
--- test.py
+++
@@ -1,2 +0,0 @@
-aiter = client.iter_dialogs(limit=30) if offset == 0 else aislice(client.iter_dialogs(limit=(offset + 1) * 30), 0,
-                                                                   offset * 30)
